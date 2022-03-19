fdd39a31be4b23da249e5934a51740f9c1db50d9
nhattin57 <nhattin57@gmail.com>
2022-03-19 12:04:04 +0700

    themxiu

--- DoAnWeb/DoAnWeb/Controllers/SanPhamController.cs
+++ DoAnWeb/DoAnWeb/Controllers/SanPhamController.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Net;
+using DoAnWeb.Models;
 namespace DoAnWeb.Controllers
 {
     public class SanPhamController : Controller
     {
         // GET: SanPham
+        WebSiteBanHangModel db = new WebSiteBanHangModel();
         public ActionResult SanPhamStylePartial()
         {
             return PartialView();
         }
 
+        public ActionResult XemTatCaSanPham(int? MaLoaiSP)
+        {
+            if (MaLoaiSP == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var lstSP = db.SanPhams.Where(n => n.MaLoaiSP == MaLoaiSP && n.DaXoa == false);
+            if (lstSP.Count() == 0)
+            {
+                return HttpNotFound();
+            }
+
+            return View(lstSP);
+        }
     }
 }
